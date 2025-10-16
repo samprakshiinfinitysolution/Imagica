99a9ffe29c300f0641cf6ef5f1deca6d5798bc83
Miteshyadav7 <miteshy101@gmail.com>
2025-10-16 11:23:40 +0530

    changeport

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("✅ MongoDB connected");
-    const PORT = process.env.PORT || 5000;
+      const PORT = process.env.PORT
     app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
